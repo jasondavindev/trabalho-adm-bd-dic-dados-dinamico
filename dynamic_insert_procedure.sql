4a8533f922511f068f6cdd80891e32233cd232d2
jasondavindev <jason.davin12@gmail.com>
2020-10-25 15:24:49 -0300

    implements source and target user

--- dynamic_insert_procedure.sql
+++ dynamic_insert_procedure.sql
@@ -8,43 +8,38 @@ GRANT WRITE ON
 DIRECTORY user_dir TO PUBLIC;
 
 CREATE OR REPLACE
-PROCEDURE dup_tables(user_name varchar) IS F_LOG utl_file.file_type;
-
-query_str varchar2(500);
-
-CURSOR tables_cursor IS
-SELECT
-	table_name
-FROM
-	user_tables
-WHERE
-	USER = user_name;
+PROCEDURE dup_tables(source_user varchar, target_user varchar) IS 
+	F_LOG utl_file.file_type;
+	query_str varchar2(500);
+	CURSOR tables_cursor IS
+		SELECT
+			table_name
+		FROM
+			user_tables
+		WHERE
+			USER = source_user;
 BEGIN
-	F_LOG := utl_file.fopen('USER_DIR',
-	'arq.txt',
-	'w');
+	F_LOG := utl_file.fopen('USER_DIR', 'arq.txt', 'w');
 
 FOR RESULT IN tables_cursor
 LOOP
 	SELECT
-	fn_dynamic_insert(result.table_name)
+		fn_dynamic_insert(source_user, target_user, result.table_name)
 INTO
 	query_str
 FROM
 	dual;
 
 utl_file.put_line(F_LOG, query_str);
-
 dbms_output.put_line(query_str);
 END
 LOOP;
-
 utl_file.fclose(F_LOG);
 END;
 /
 
 CREATE OR REPLACE
-FUNCTION fn_dynamic_insert(source_table varchar) RETURN varchar2 IS columns_str varchar(255);
+FUNCTION fn_dynamic_insert(source_user varchar, target_user varchar, source_table varchar) RETURN varchar2 IS columns_str varchar(255);
 
 query_str varchar2(500);
 BEGIN
@@ -61,7 +56,8 @@ WHERE
 GROUP BY
 	table_name;
 
-query_str := 'INSERT INTO ' || source_table || '(' || columns_str || ')' || 'SELECT ' || columns_str || ' FROM ' || source_table || ';';
+query_str := 'INSERT INTO ' || target_user || '.' || source_table || '(' || columns_str || ')' || ' SELECT ' || columns_str || ' FROM ' || source_user || '.' || source_table || ';';
 
 RETURN(query_str);
 END;
+/
